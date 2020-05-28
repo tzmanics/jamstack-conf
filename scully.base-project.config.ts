f8f96e1b1df1190f6c5a0b3ab1f9650d86058e17
Tara Z. Manicsic <tzmanics@gmail.com>
2020-05-28 12:31:09 -0400

    adds scully and netlify command update

---
+++ scully.base-project.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "base-project",
+  outDir: './dist/static',
+  routes: {
+  }
+};
